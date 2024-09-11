258beb43042069e56b6520059c501421b86a4672
Thet Htar Thu Zar <thethtarthuzar648@gmail.com>
2024-09-11 19:29:01 +0630

    -

--- FtpExample/Controllers/FtpController.cs
+++ FtpExample/Controllers/FtpController.cs
@@ -29,6 +29,20 @@ namespace FtpExample.Controllers
             }
         }
 
+        [HttpPost("CreateDirectory")]
+        public async Task<IActionResult> CreateDirectory(string directory)
+        {
+            try
+            {
+                bool isCreateSuccessful = await _ftpService.CreateDirectoryAsync(directory);
+                return Ok(isCreateSuccessful);
+            }
+            catch (Exception ex)
+            {
+                throw new Exception(ex.Message);
+            }
+        }
+
         [HttpPost("UploadFile")]
         public async Task<IActionResult> UploadFile([FromForm] FtpRequestModel requestModel)
         {
